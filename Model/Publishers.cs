4a44de1554d9b934b50f0c9da01c3a7ae57bd558
xuqiang <1290272597@qq.com>
2018-06-05 08:34:51 +0800

    1

---
+++ Model/Publishers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cn.itcast.bookshop.Model
+{
+    [Serializable]
+    public partial class Publishers
+    {
+        /// <summary>
+        /// Publishers:实体类(属性说明自动提取数据库字段的描述信息)
+        /// </summary>
+       
+     
+            public Publishers()
+            { }
+            #region Model
+            private int _id;
+            private string _name;
+            /// <summary>
+            /// 
+            /// </summary>
+            public int Id
+            {
+                set { _id = value; }
+                get { return _id; }
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string Name
+            {
+                set { _name = value; }
+                get { return _name; }
+            }
+            #endregion Model
+        }
+    }
